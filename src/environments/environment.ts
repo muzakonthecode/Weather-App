8d00ab32c9ed61809b46f6aa029b4b8270a28467
Muhammad Zaki <mahmadzekoo@gmail.com>
2022-09-02 01:08:19 +0200

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,11 +4,11 @@
 
 export const environment = {
   production: false,
-  weatherApiBaseUrl: 'https://weatherapi-com.p.rapidapi.com/current.json',
-  XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
-  XRapidAPIHostHeaderValue: 'weatherapi-com.p.rapidapi.com',
-  XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
-  XRapidAPIKeyHeaderValue: '594913fb0amshdbe44dca31547dep139744jsn533080feab4d',
+  weatherApiBaseUrl: '',
+  XRapidAPIHostHeaderName: '',
+  XRapidAPIHostHeaderValue: '',
+  XRapidAPIKeyHeaderName: '',
+  XRapidAPIKeyHeaderValue: '',
 };
 
 /*
